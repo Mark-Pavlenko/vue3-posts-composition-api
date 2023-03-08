c9ca403628d49c223de46139285b92cbb62f248c
Marko Pavlenko <mark.pavlenko@megadevllc.com>
2023-03-08 17:00:34 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <TimeLine />
+    </div>
+  </section>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TimeLine from './components/Timeline.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    TimeLine
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a>Today</a>
+      <a>This week</a>
+      <a>This month</a>
+    </span>
+  </nav>
+    <h1>Timeline</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TimeLine',
+
+  setup () {
+    console.log('component is setup')
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
