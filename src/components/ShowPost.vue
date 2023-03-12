0d8dbda6854c41683bba5356c0c32dbdfef15830
Marko Pavlenko <marko.pavlenko@nure.ua>
2023-03-12 23:17:07 +0300

    feat: make page of single post displaying

---
+++ src/components/ShowPost.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    Show post
+    {{post}}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRoute } from 'vue-router'
+import { useStore } from '@/store'
+
+export default defineComponent({
+  name: 'ShowPost',
+  setup (props) {
+    const store = useStore()
+    const postId = useRoute().params.id as string
+    const post = store.getState().posts.all.get(postId)
+    return {
+      post
+    }
+  }
+})
+</script>
+
+<style scoped>
+</style>
